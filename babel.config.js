842ace91007c6fc147ac1ef376d32b468648ec01
marshennikovaolga <marshennikova@gmail.com>
2024-03-18 19:32:47 +0400

    prettier

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
-module.exports = function(api) {
-  api.cache(true);
-  let plugins = [];
+module.exports = function (api) {
+  api.cache(true)
+  const plugins = []
 
   return {
-      presets: [
-        ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-        'nativewind/babel',
-      ],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
 
     plugins,
-  };
-};
+  }
+}
